266f515b2c1b706ca01a1f8942f7d2c2dad281ac
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-07-18 14:02:29 -0400

    divan

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,26 +1,16 @@
-use criterion::{criterion_group, criterion_main, Criterion};
 use std::hint::black_box;
-
 use biter::{Biter,MutBiter};
-fn criterion_benchmark(c: &mut Criterion) {
-    //last one is about 9000 elements from the end
+fn main() {divan::main();}
+
+#[divan::bench]
+fn bit_iter() {
     let zend: Vec<u64> = (0..1000).rev().chain(core::iter::repeat(0).take(9000)).collect();
-
-    // 4.8814 µs per iter
-    c.bench_function("biter", |b| {
-        b.iter(|| {
-            let mut set_bits = 0;
-            Biter::from(&zend).for_each(|bit| set_bits+=bit as usize);
-            })
-    });
-
-    // 24.660 µs per iter
-    c.bench_function("biter_mut", |b| {
-        b.iter(|| {MutBiter::from(zend.clone()).for_each(|mut bit| *bit=true)})
-    });
-
-
+    let mut set_bits=0;
+    Biter::from(&zend).for_each(|bit| set_bits+=bit as usize);
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+#[divan::bench]
+fn bit_iter_mut() {
+    let zend: Vec<u64> = (0..1000).rev().chain(core::iter::repeat(0).take(9000)).collect();
+    MutBiter::from(zend).for_each(|mut bit| *bit=true)
+}
