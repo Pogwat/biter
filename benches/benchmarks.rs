9ce2a76ab6f0ee73b4ef94b1e83789511c70e4be
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-07-20 04:48:47 -0400

    criterion package

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,31 +1,25 @@
 use biter::{Biter,MutBiter};
-fn main() {divan::main();}
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
 
-use divan::{Bencher, black_box};
-
-#[divan::bench]
-fn bit_iter(bencher: Bencher) {
-    bencher
-        .with_inputs(|| {
-             let zend: Vec<u64> = (0..1000).rev().chain(core::iter::repeat(0).take(9000)).collect();
-             zend
-        })
-        .bench_refs(|zend| {
+fn bit_iter(c: &mut Criterion) {
+    let zend: Vec<u64> = (0..1000).rev().chain(core::iter::repeat(0).take(9000)).collect();
+    c.bench_function("bit_iter", |b|
+        b.iter(|| {
             let mut set_bits = 0;
-            Biter::from(zend).for_each(|bit| set_bits += bit as usize);
+            Biter::from(&zend).for_each(|bit| set_bits += bit as usize);
             black_box(set_bits);
-        });
+        }));
 }
 
-#[divan::bench]
-fn bit_iter_mut(bencher: Bencher) {
-    bencher
-        .with_inputs(|| {
-             let zend: Vec<u64> = (0..1000).rev().chain(core::iter::repeat(0).take(9000)).collect();
-             zend
-        })
-        .bench_values(|zend| {
-            MutBiter::from(zend).for_each(|mut bit| {*black_box(bit) = true;
-            });
-        });
+
+fn bit_iter_mut(c: &mut Criterion) {
+    let zend: Vec<u64> = (0..1000).rev().chain(core::iter::repeat(0).take(9000)).collect();
+    c.bench_function("bit_iter_mut", |b|
+        b.iter(|| {
+            MutBiter::from(zend.clone()).for_each(|bit| {*black_box(bit) = true});
+        }));
 }
+
+criterion_group!(biters, bit_iter,bit_iter_mut);
+criterion_main!(biters);
